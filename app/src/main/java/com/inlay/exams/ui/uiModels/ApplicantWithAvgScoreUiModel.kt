1483fa59f2441186a3d017143bf5d6d393c699d8
In-Lay <flamefreeme@gmail.com>
2023-05-17 19:07:31 +0300

    Work commit

--- app/src/main/java/com/inlay/exams/ui/uiModels/ApplicantWithAvgScoreUiModel.kt
+++ app/src/main/java/com/inlay/exams/ui/uiModels/ApplicantWithAvgScoreUiModel.kt
@@ -2,4 +2,7 @@ package com.inlay.exams.ui.uiModels
 
 import com.inlay.exams.data.database.entities.Applicant
 
-data class ApplicantWithAvgScoreUiModel(val applicant: Applicant?, val averageScore: Int)
+data class ApplicantWithAvgScoreUiModel(
+    val applicant: Applicant?,
+    val averageScore: Int
+)
